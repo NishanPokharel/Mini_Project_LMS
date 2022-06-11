0f996e8ca8e69767be4bdc9dc1b9eb34aa662037
NishanPokharel <67104679+NishanPokharel@users.noreply.github.com>
2022-06-11 18:19:10 +0545

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="./style.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
     <!-- <link rel="stylesheet" href="./style.css"> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -33,10 +33,10 @@
     </nav><br><br>
     <div class="row">
         <!-- <center> -->
-            <div class="col-md-8" id="main-content">
+            <div class="col-md-6" id="main-content">
             <center><h3>Admin Login Form </h3></center>
             <!-- <form action="Register.php" class="form-layout"method="post"> -->
-            <form action="" class="form-layout"method="post">
+            <form action="admin_dashboard.php" class="form-layout"method="post">
                 <div class="form-group">
                     <label for="emaillabel">E-Mail</label>
                     <input type="text" name="email" class="form-control" required>
