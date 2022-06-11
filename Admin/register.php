e11aef46499408363f278295625d1a6e4d401b79
NishanPokharel <67104679+NishanPokharel@users.noreply.github.com>
2022-06-11 18:18:50 +0545

    Update register.php

--- Admin/register.php
+++ Admin/register.php
@@ -1,11 +1,11 @@
 <?php
     $connection = mysqli_connect("localhost","root","");
     $db = mysqli_select_db($connection,"miniproject");
-    $query = "insert into admins values('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]',$_POST[mobilenumber],'$_POST[address]')";
+    $query = "insert into admins values('','$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]',$_POST[mobilenumber],'$_POST[address]')";
     $query_run = mysqli_query($connection, $query);
 ?>
 
 <script type="text/javascript">
     alert("Registration Successful")
-    window.location.href = "Admin/index.php";
+    window.location.href = "./index.php";
 </script>
